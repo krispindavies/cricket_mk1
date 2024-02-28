39fa37f9524abb81e52ca2dd4b817d5aa88d94a0
Krispin Davies <krispin.davies@live.ca>
2024-02-27 23:02:46 -0500

    Tested out hello world function to check package build.

---
+++ src/hello_world.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 Krispin Davies
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+  std::cout << "Hello world." << std::endl;
+}
